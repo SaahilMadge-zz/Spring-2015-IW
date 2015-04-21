1a8668db7aec5d73a86a2f0521ce2562c03379ef
SaahilM <smadge@princeton.edu>
2015-04-20 22:52:22 -0400

    Sanitized data when prices were unavailable

--- pandasTest.py
+++ pandasTest.py
@@ -10,44 +10,47 @@ def findPercentile(marketCap, marketCapArray):
 			return i
 	return 10  # if it's the highest
 
-df = pandas.read_csv('sp500data.csv')
+df = pandas.read_csv('sp500data2.csv')
 
-tsymList = sorted(list(set(df['TICKER'])))
-print tsymList
+permnoList = sorted(list(set(df['PERMNO'])))
+# skip certain companies b/c their price data is not full
+#permnoToSkip = [10942, 11896]
+#permnoList = [x for x in permnoList if x not in permnoToSkip]
+print permnoList
 companyChanges = {}
-tsymList.pop(0)    #remove nan at the beginning
-print len(tsymList)
+#permnoList.pop(0)    #remove nan at the beginning
+print len(permnoList)
 
 f = open('pythonlist.txt', 'w+')
 
-companiesToSkip = ['WBA'] # companies that shouldn't be in this data
-for company in companiesToSkip:
-	tsymList.remove(company)
+#companiesToSkip = ['WBA'] # companies that shouldn't be in this data
+#for company in companiesToSkip:
+#	permnoList.remove(company)
 # marketCapArray = numpy.array([])
 marketCapArray = []
 marketCapDict = {}
 
-for i in range(len(tsymList)):
-	if(tsymList[i] in companiesToSkip):
-		continue
+for i in range(len(permnoList)):
+	# if(permnoList[i] in companiesToSkip):
+		# continue
 
-	compData = df[df['TICKER'] == tsymList[i]]
+	compData = df[df['PERMNO'] == permnoList[i]]
 	marketShare = abs(list(compData['PRC'])[-5]) * abs(list(compData['SHROUT'])[-5])
 	# print list(compData['PRC'])[-1]
-	#f.write('%s:%f\n' % (tsymList[i], marketShare))
-	print '%s: %f' % (tsymList[i], marketShare)
+	#f.write('%s:%f\n' % (permnoList[i], marketShare))
+	print '%s: %f' % (permnoList[i], marketShare)
 	marketCapArray.append(marketShare)
-	marketCapDict[tsymList[i]] = [marketShare]
+	marketCapDict[permnoList[i]] = [marketShare]
 
 # print marketCapArray
 for i in range(1,11):
 	print "%d percentile: %f" % (i*10, numpy.percentile(marketCapArray, i*10))
 
-for company in tsymList:
-	marketCap = marketCapDict[company][0]
+for permno in permnoList:
+	marketCap = marketCapDict[permno][0]
 	percentile = findPercentile(marketCap, marketCapArray)
-	print '%s: %f\tpercentile:%d' % (company, marketCap, percentile)
-	marketCapDict[company].append(percentile)
+	print '%s: %f\tpercentile:%d' % (permno, marketCap, percentile)
+	marketCapDict[permno].append(percentile)
 
 alpha = 0.15  # percent change needed to be significant
 
@@ -59,9 +62,9 @@ meanReturnArray = []
 for i in range(10):
 	meanReturnArray.append([])
 
-def findChangeIndices(companyName):
+def findChangeIndices(permno):
 	
-	priceList = numpy.array(list(df[df['TICKER'] == str(companyName)]['PRC']))
+	priceList = numpy.array(list(df[df['PERMNO'] == permno]['PRC']))
 	priceList = priceList[~numpy.isnan(priceList)]
 
 	changeList = []
@@ -78,31 +81,52 @@ def findChangeIndices(companyName):
 
 	return len(changeList)
 
-def findMean(companyName):
-	priceList = numpy.array(list(df[df['TICKER'] == str(companyName)]['PRC']))
-	priceList = priceList[numpy.logical_not(numpy.isnan(priceList))]
+def isFloat(x):
+	try:
+		a = float(x)
+	except ValueError:
+		return False
+	else:
+		return True
+
+def findMeanAndStdDev(permno):
+	# get returns and find return
+	#priceList = numpy.array(list(df[df['PERMNO'] == permno]['PRC']))
+	# print type(returnList)
+	returnList = numpy.array(list(df[df['PERMNO'] == permno]['RET']))
+	returnList = numpy.array([x for x in returnList if isFloat(x)])
+	#print returnList
+	# print type(returnList2)
+	returnList = returnList.astype('float')
+	returnList = returnList[numpy.logical_not(numpy.isnan(returnList))]
+
+	# find the % return
 
 	# get the mean as a %
-	percentageMean = numpy.mean(priceList)#/float(priceList[0])
-	return percentageMean
+	percentageMean = numpy.mean(returnList)#/float(priceList[0])
+	percentageStdDev = numpy.std(returnList)
+	return (percentageMean, percentageStdDev)
 
-def findStdDev(companyName):
-	priceList = numpy.array(list(df[df['TICKER'] == str(companyName)]['PRC']))
-	priceList = priceList[numpy.logical_not(numpy.isnan(priceList))]
+# def findStdDev(permno):
+# 	priceList = numpy.array(list(df[df['PERMNO'] == permno]['PRC']))
+# 	priceList = priceList[numpy.logical_not(numpy.isnan(priceList))]
 
-	# get the stddev as a %
-	percentageStdDev = numpy.std(priceList)#/float(priceList[0])
-	return percentageStdDev
+# 	# get the stddev as a %
+# 	percentageStdDev = numpy.std(priceList)#/float(priceList[0])
+# 	return percentageStdDev
 
 percentileChangesArray = [0]*10
 
-for companyName in tsymList:
+for permno in permnoList:
 	# get market cap, find number of changes, and also find standard deviation and mean return
-    percentile = marketCapDict[companyName][1]
-    numChanges = findChangeIndices(companyName)
-    mean = findMean(companyName)
-    stdDev = findStdDev((companyName))
-    print "%s: %d\t%d changes\t %f mean\t %f std" % (companyName, percentile, numChanges, mean, stdDev)
+    percentile = marketCapDict[permno][1]
+    numChanges = findChangeIndices(permno)
+    meanAndStdDev = findMeanAndStdDev(permno)
+    mean = meanAndStdDev[0]
+    stdDev = meanAndStdDev[1]
+    #mean = findMean(permno)
+    # stdDev = findStdDev((permno))
+    print "%s: %d\t%d changes\t %f mean\t %f std" % (permno, percentile, numChanges, mean, stdDev)
     percentileChangesArray[percentile-1] += numChanges
     standardDeviationArray[percentile-1].append(stdDev)
     meanReturnArray[percentile-1].append(mean)
@@ -120,53 +144,53 @@ print "meanReturnArray"
 print meanReturnArray
 
 
-countryRateChangeDict = {}
-def getCurrencyData():
-	global countryRateChangeDict
-	df2 = pandas.read_csv('basicCurrencyData.csv')
+# countryRateChangeDict = {}
+# def getCurrencyData():
+# 	global countryRateChangeDict
+# 	df2 = pandas.read_csv('basicCurrencyData.csv')
 
-	countries = numpy.delete(df2.columns.values, 0)
-	print countries
-	# test = df[countries[0]]
-	# test = list(test[np.logical_not(np.isnan(test))])
-	# print test
+# 	countries = numpy.delete(df2.columns.values, 0)
+# 	print countries
+# 	# test = df[countries[0]]
+# 	# test = list(test[np.logical_not(np.isnan(test))])
+# 	# print test
 
-	for country in countries:
-		fxRates = df2[country]
-		fxRates = list(fxRates[numpy.logical_not(numpy.isnan(fxRates))])
+# 	for country in countries:
+# 		fxRates = df2[country]
+# 		fxRates = list(fxRates[numpy.logical_not(numpy.isnan(fxRates))])
 
-		alpha = 0.05  # percent change needed to be significant
+# 		alpha = 0.05  # percent change needed to be significant
 
-		changeList = []
+# 		changeList = []
 
-		lastRate = abs(fxRates[0])
-		counter = 0
-		for rate in fxRates:
-			absRate = abs(rate)
-			# print "price:%f\t%r" % (absPrice, absPrice < (1-alpha)*lastPrice or absPrice > (1+alpha)*lastPrice)
-			if (absRate < (1-alpha)*lastRate or absRate > (1+alpha)*lastRate):
-				counter += 1
-				# if (country == 'china'):
-				# 	print "last:%s  new:%s" % (absRate, lastRate)
-			lastRate = absRate
+# 		lastRate = abs(fxRates[0])
+# 		counter = 0
+# 		for rate in fxRates:
+# 			absRate = abs(rate)
+# 			# print "price:%f\t%r" % (absPrice, absPrice < (1-alpha)*lastPrice or absPrice > (1+alpha)*lastPrice)
+# 			if (absRate < (1-alpha)*lastRate or absRate > (1+alpha)*lastRate):
+# 				counter += 1
+# 				# if (country == 'china'):
+# 				# 	print "last:%s  new:%s" % (absRate, lastRate)
+# 			lastRate = absRate
 
-		countryRateChangeDict[country] = counter
+# 		countryRateChangeDict[country] = counter
 
-	print countryRateChangeDict
-	print sum(countryRateChangeDict.values())
+# 	print countryRateChangeDict
+# 	print sum(countryRateChangeDict.values())
 
-getCurrencyData()
+# getCurrencyData()
 
 # plot
 decileArray = []
 for i in range(10):
-	decileArray.append('%d decile' % (i+1))
+	decileArray.append('%d' % (i+1))
 
 # graph of percentile changes by decile
 plt.figure(1)
-# add the currency data to it
-percentileChangesArray.append(sum(countryRateChangeDict.values()))
-decileArray.append("FX rates")
+# # add the currency data to it
+# percentileChangesArray.append(sum(countryRateChangeDict.values()))
+# decileArray.append("FX rates")
 x_pos = numpy.arange(len(decileArray))
 
 print len(x_pos)
@@ -179,16 +203,16 @@ plt.ylabel('Days with more than %f%% change in price' % (alpha*100))
 # graph of standard deviation by decile
 plt.figure(2)
 plt.subplot(2,1,1)
-# add currency data to it
-standardDeviationArray.append(numpy.std(countryRateChangeDict.values()))
-meanReturnArray.append(numpy.mean(countryRateChangeDict.values()))
+# # add currency data to it
+# standardDeviationArray.append(numpy.std(countryRateChangeDict.values()))
+# meanReturnArray.append(numpy.mean(countryRateChangeDict.values()))
 plt.bar(x_pos, standardDeviationArray)
 plt.xticks(x_pos, decileArray)
 plt.ylabel('Standard Deviation')
 plt.title('Standard Deviation vs stock decile')
 plt.subplot(2,1,2)
 plt.plot(standardDeviationArray, meanReturnArray, 'ro')
-#plt.xticks(standardDeviationArray, decileArray)
+plt.xticks(standardDeviationArray, decileArray)
 plt.ylabel('Mean Return')
 plt.title('Return vs Standard Deviation')
 
